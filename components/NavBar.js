eccd1dec2dc6e5c1f5bd1f652897459c79f066b2
Jolllly <jolllly@foxmail.com>
2024-05-03 14:26:43 -0400

    <Feat>: display user home after login

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,24 +1,25 @@
 import Link from "next/link";
 import {GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from "../firebase";
+import { useState, useEffect } from "react";
 
 const provider = new GoogleAuthProvider();
 
 const signInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
     const user = result.user;
-    console.log(user);
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
+    
+    if (user.email && user.email.endsWith('@cornell.edu')) {
+      console.log('Authenticated successfully', user);
+    } else {
+      await signOut(auth);
+      throw new Error('Please use a Cornell University email to sign in.');
+    }
+
   } catch (error) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.customData.email;
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
+    console.error('Login failed:', error.message);
+    alert(error.message);
   }
 };
 
@@ -33,6 +34,11 @@ const signOutGoogle = async () => {
 
 
 const NavBar = () => {
+  const [currentUser, setCurrentUser] = useState(auth.currentUser);
+
+  auth.onAuthStateChanged(user => {
+    setCurrentUser(user); // Update user state whenever the auth state changes
+  });
 
   return (
     <nav
@@ -50,63 +56,44 @@ const NavBar = () => {
         <div className="navbar-start"></div>
       </div>
       <div className="navbar-end">
-        <Link href="/app/home">
           {/* CS5356 TODO 1a. Navigation
 
-          Create a new page at /app/home to be the Instructor Home Page.
-          This page should only be visible by signed in users.
-          If a signed-out user tries to access the page, they should
-          be redirected to the login page to complete signing in.
+Create a new page at /app/home to be the Instructor Home Page.
+This page should only be visible by signed in users.
+If a signed-out user tries to access the page, they should
+be redirected to the login page to complete signing in.
 
-          The Instructor Home Page should have 2 parts, similar to your 
-          wireframe.
-          1. The page should contain one section to display a short form
-          allowing them to create a class code by making a 
-          POST /api/class-codes (Completed in step 3a). When a class code has been created, this 
-          page should re-fetch the user's class codes.
-          
-          2. The page should contain another section to display a list or
-          table of the user's class codes. When the page loads for the first
-          time, make a GET /api/class-codes (Completed in step 3a) to fetch the list of class codes.
-          */}
-          <a className="navbar-item">User Home</a>
-        </Link>
+The Instructor Home Page should have 2 parts, similar to your 
+wireframe.
+1. The page should contain one section to display a short form
+allowing them to create a class code by making a 
+POST /api/class-codes (Completed in step 3a). When a class code has been created, this 
+page should re-fetch the user's class codes.
+
+2. The page should contain another section to display a list or
+table of the user's class codes. When the page loads for the first
+time, make a GET /api/class-codes (Completed in step 3a) to fetch the list of class codes.
+*/}
+        
         <div className="navbar-item">
           <div className="buttons">
-            {/* CS5356 TODO 1b. Authentication
-
-              Finish configuring NextAuth by editing pages/api/auth/[...nextAuth].js.
-              Then get the user's session and check if they are signed in.
-              https://next-auth.js.org/getting-started/client#usesession
-
-              Display a button here to "Sign In" in if the user is not currently signed in,
-              i.e they don't have an active session. It should send the user to /api/auth/signin
-              https://next-auth.js.org/getting-started/client#signin
-
-              If the user is signed in and they have an active session, the button 
-              should display "Sign out", and it should direct the user to the /api/auth/signout page
-              https://next-auth.js.org/getting-started/client#signout
-             */}
-            {auth.currentUser ? (
-              <div>
-                <p className="navbar-item">Signed in as: {auth.currentUser.displayName}</p>
-              </div>
-            ) : (
-              <div>
+            {currentUser ? (
+              <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-between' }}>
+                {/* <p className="navbar-item">{currentUser.displayName}</p> */}
+                <Link href="/app/home">
+                  <a className="navbar-item">My contributions</a>
+                </Link>
                 <button className="button is-primary" onClick={() => signOutGoogle()}>
                   Sign Out
                 </button>
-              <button className="button is-primary" onClick={() => signInWithGoogle()}>
-                Sign In
-              </button>
+              </div>
+            ) : (
+              <div>
+                <button className="button is-primary" onClick={() => signInWithGoogle()}>
+                  Sign In
+                </button>
               </div>
             )}
-            {/* <button
-              className="button is-primary"
-              onClick={() => signInWithPopup()}
-             >
-              Sign In
-             </button> */}
           </div>
         </div>
       </div>
