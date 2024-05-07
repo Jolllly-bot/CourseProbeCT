76564aeb9eb76e648e02ab8f3dd8438de5539643
bronyazh <61338377+bronyazh@users.noreply.github.com>
2024-05-07 00:27:05 -0400

    final ver for index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import ClassLink from "../components/ClassLink";
-import { getCourses, addCourse } from '../services/firestore/courses';
 import { useState, useEffect } from "react";
+import { getCourses, addCourse } from '../services/firestore/courses';
+import { auth } from "../firebase"; 
 
 export default function Home() {
   const [courses, setCourses] = useState([]);
   const [filter, setFilter] = useState(new Set());
   const [courseName, setCourseName] = useState('');
   const [courseCode, setCourseCode] = useState('');
-  const [searchText, setSearchText] = useState('');  // State to hold the search text
-
+  const [searchText, setSearchText] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
       const fetchCourses = async () => {
@@ -19,10 +19,21 @@ export default function Home() {
               console.error("Failed to fetch courses:", error);
           }
       };
-
+      auth.onAuthStateChanged(user => {
+        if (user) {
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
+        }
+    });
       fetchCourses();
   }, []);
 
+  const handleCourseCodeChange = (e) => {
+    const upperCaseCode = e.target.value.toUpperCase();
+    setCourseCode(upperCaseCode);
+  };
+  
   const handleFilterToggle = (dept) => {
       const newFilter = new Set(filter);
       if (newFilter.has(dept)) {
@@ -34,51 +45,57 @@ export default function Home() {
   };
 
   const handleSubmit = async (event) => {
-      console.log("submit");
-      event.preventDefault();
-      try {
-          const courseData = {
-              name: courseName,
-              code: courseCode,
-          };
-          await addCourse(courseData);
-          alert('Course added successfully!');
-          setCourseName('');
-          setCourseCode('');
-      } catch (error) {
-          console.error('Error adding course:', error);
-          alert('Failed to add course.');
-      }
+    event.preventDefault();
+    if (!isLoggedIn) {
+        alert('Please login first.');
+        return;
+    }
+    if (!courseCode.trim() || !courseName.trim()) {
+        alert('Please enter both course code and course name before submitting.');
+        return;
+    }
+    try {
+        const courseData = { name: courseName, code: courseCode };
+        await addCourse(courseData);
+        alert('Course added successfully!');
+        setCourses(prevCourses => [...prevCourses, {...courseData, id: new Date().getTime()}]);
+        setCourseName('');
+        setCourseCode('');
+    } catch (error) {
+        console.error('Error adding course:', error);
+        alert('Failed to add course.');
+    }
   };
 
   const filteredCourses = courses.filter(course => 
     (filter.size === 0 || Array.from(filter).some(dept => course.code.startsWith(dept))) &&
-    course.name.toLowerCase().includes(searchText.toLowerCase())  // Filter by search text
+    course.name.toLowerCase().includes(searchText.toLowerCase())
   );
 
   return (
     <div>
       <main>
-        <section className="hero is-medium is-link">
-          <div className="hero-body columns is-vcentered">
+        <section className="hero is-link" style={{ position: 'relative', overflow: 'hidden', width: '100vw', height: '56.25vw', maxHeight: '100vh' }}>
+          <iframe 
+            src="https://www.youtube.com/embed/n8Y34Axq2EM?enablejsapi=1&autoplay=1&loop=1&controls=0&rel=0&wmode=transparent&showinfo=0&mute=1&autohide=0&modestbranding=1&playsinline=1&hd=1&vq=hd1080&disablekb=1&origin=https%3A%2F%2Ftech.cornell.edu&widgetid=1&playlist=n8Y34Axq2EM" 
+            style={{ position: 'absolute', top: '0', left: '0', width: '100vw', height: '56.25vw', border: 'none', objectFit: 'cover' }}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <div className="hero-body columns is-vcentered" style={{ position: 'relative', zIndex: '10' }}>
             <div className="column">
-              <p className="title">Ask and share questions during class</p>
-              <p className="subtitle">
-                Quickly  a URL where you can post questions during class
-                and see what other users are saying
+              <p className="title" style={{ fontSize: '2em' }}>Explore Cornell Tech Course Reviews</p>
+              <p className="subtitle" style={{ fontSize: '1.5em' }}>
+                Discover what students say about their courses at Cornell Tech
               </p>
-              <ClassLink />
-            </div>
-            <div className="column">
-              <figure>
-                <img src="https://dummyimage.com/640x360/fff/aaa" />
-              </figure>
+              <a href="https://classes.cornell.edu/browse/roster/SP24" className="button is-success" style={{ height: '2.5em' }}>View Courses at Cornell</a>
             </div>
           </div>
         </section>
-        <div className="columns is-centered"style={{ marginTop: '20px' }}>
+        <div className="columns is-centered" style={{ marginTop: '20px' }}>
           <div className="column is-3">
-            <input type="text" className="input" style={{ height: '2.5em' }} value={courseCode} onChange={e => setCourseCode(e.target.value)} placeholder="Course Code" />
+            <input type="text" className="input" style={{ height: '2.5em' }} value={courseCode} onChange={handleCourseCodeChange} placeholder="Course Code" />
           </div>
           <div className="column is-3">
             <input type="text" className="input" style={{ height: '2.5em' }} value={courseName} onChange={e => setCourseName(e.target.value)} placeholder="Course Name" />
@@ -89,66 +106,32 @@ export default function Home() {
         </div>
         <div className="buttons has-addons is-centered" style={{ marginBottom: '20px' }}>
           {['CS', 'ECE', 'INFO', 'LAW', 'NBAY', 'ORIE', 'TECH'].map(dept => (
-              <button
-                key={dept}
-                onClick={() => handleFilterToggle(dept)}
-                className={`button ${filter.has(dept) ? 'is-success' : ''}`}
-                style={{ fontSize: '1.1rem', fontWeight: 'bold', margin: '5px' }}
-              >
-                {dept}
-              </button>
+            <button key={dept} onClick={() => handleFilterToggle(dept)} className={`button ${filter.has(dept) ? 'is-success' : ''}`} style={{ fontSize: '1.1rem', fontWeight: 'bold', margin: '5px' }}>
+              {dept}
+            </button>
           ))}
         </div>
-        <div style={{ textAlign: 'center', marginTop: '10px' }}> {/* Add a search input */}
-          <input
-            type="text"
-            className="input"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Search Course Name"
-            style={{ height: '2.5em', width: '500px' }} 
-          />
+        <div style={{ textAlign: 'center', marginTop: '10px' }}>
+          <input type="text" className="input" value={searchText} onChange={(e) => setSearchText(e.target.value)} placeholder="Search Course Name" style={{ height: '2.5em', width: '500px' }} />
         </div>
-        <section className="section" style={{ overflowY: 'auto', height: '600px' }}>
-  <ul>
-    {filteredCourses.map(course => (
-      <li key={course.id} style={{ 
-        padding: '10px', 
-        backgroundColor: '#f0f0f0', 
-        margin: '10px', 
-        borderRadius: '10px',
-        display: 'flex',  
-        alignItems: 'center'  
-      }}>
-        <strong style={{ 
-          fontSize: '1.8em', 
-          flex: '1 0 20%'  
-        }}>{course.code}</strong>
-        <span style={{
-          fontSize: '1.8em', 
-          flex: '1 0 60%'  
-        }}>{course.name}</span>
-      </li>
-    ))}
-  </ul>
-</section>
-        <section className="hero is-medium">
-          <div className="hero-body columns is-vcentered">
-            <div className="column">
-              <figure>
-                <img src="https://dummyimage.com/640x360/fff/aaa" />
-              </figure>
-            </div>
-            <div className="column">
-              <p className="title">
-                Share questions with the class using a custom URL
-              </p>
-              <p className="subtitle">
-                Create your own temporary URL where you can save questions and
-                view them in real time.
-              </p>
-            </div>
-          </div>
+          <section className="section custom-scrollbar" style={{ overflowY: 'auto', height: '600px' }}>
+          <ul>
+            {filteredCourses.map(course => (
+              <li key={course.id} style={{ 
+                padding: '10px', 
+                backgroundColor: '#f0f0f0', 
+                margin: '10px', 
+                borderRadius: '10px',
+                display: 'flex',  
+                alignItems: 'center'  
+              }}>
+                <a href={`/app/${course.id}`} style={{ textDecoration: 'none', color: 'inherit', flex: '1 0 20%' }}>
+                  <strong style={{ fontSize: '1.8em' }}>{course.code}</strong>
+                </a>
+                <span style={{ fontSize: '1.8em', flex: '1 0 60%' }}>{course.name}</span>
+              </li>
+            ))}
+          </ul>
         </section>
       </main>
     </div>
